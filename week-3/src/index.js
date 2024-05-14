228eb3b52d15669ac67c440f1a39605fc81a0aa6
Clark Merrick <clark@clarkmerrick.com>
2024-05-13 18:07:48 -0700

    remove code
    removed random response since HTTP GET has new route to origin instead

--- week-3/src/index.js
+++ week-3/src/index.js
@@ -1,23 +1,5 @@
 export default {
 	async fetch(request, env, ctx) {
-		const randomResponse = {
-			1: "You used GET!",
-			2: "You're a go-GETter",
-			3: "Thank you for using HTTP GET",
-			4: "Next time try POST",
-		}
-
-		function getRandomInt(min, max) {
-      		const minCeiled = Math.ceil(min);
-      		const maxFloored = Math.floor(max);
-      		return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-    	}
-
-		// generate random value from function and print to console
-		let randomValue = getRandomInt(1, 4);
-    	console.log("random value: " + randomValue);
-		console.log(request.cf.botManagement.score)
-
 		// if bot score is less than 30
 		if (request.cf.botManagement.score < 30){
 			const remote = "https://httpbin.org/get";
